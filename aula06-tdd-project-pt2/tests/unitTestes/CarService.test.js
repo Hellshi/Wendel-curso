c93d78d68524081fcf9df30f75c3b78cba8b6eb6
hellshi <hcslimaa@gmail.com>
2023-01-17 18:34:44 -0300

    feat: test structure

---
+++ aula06-tdd-project-pt2/tests/unitTestes/CarService.test.js
@@ -0,0 +1,10 @@
+const { describe, it,  } = require('mocha');
+const assert = require('assert');
+const request = require('supertest');
+
+describe('Car Services Suite Tests', () => {
+        it('', async() => {
+           const string = 'ok'
+            assert.deepStrictEqual(string, 'ok')
+         })
+})
