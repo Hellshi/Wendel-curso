22d172408d16f7923ad954077af4d504ac66882b
hellshi <hcslimaa@gmail.com>
2023-02-27 13:31:10 -0300

    feat: adds browser and console implementation

---
+++ modulo06/creational/02.abstract-factory/shared/data.mjs
@@ -0,0 +1,267 @@
+export const database =[
+    {
+      "index": 0,
+      "guid": "1b679597-bdd4-4ea7-ab73-7e1296ad9707",
+      "isActive": false,
+      "balance": "$1,178.42",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Mollie Turner",
+      "gender": "female",
+      "company": "LIMOZEN",
+      "email": "mollieturner@limozen.com",
+      "phone": "+1 (986) 584-3274",
+      "address": "603 Hart Street, Bartley, Utah, 271",
+      "about": "Veniam magna ullamco laborum occaecat qui aliqua labore eiusmod. Amet et cillum exercitation irure enim ut culpa ea tempor. Ea sit quis fugiat culpa elit ullamco nostrud ullamco qui consectetur.\r\n",
+      "registered": "2017-04-10T05:38:14 +03:00",
+      "latitude": -4.314665,
+      "longitude": 21.354575,
+      "tags": [
+        "pariatur",
+        "aliqua",
+        "incididunt",
+        "adipisicing",
+        "minim",
+        "enim",
+        "voluptate"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Alyssa Barr"
+        },
+        {
+          "id": 1,
+          "name": "Patterson Morgan"
+        },
+        {
+          "id": 2,
+          "name": "Nichole Hughes"
+        }
+      ],
+      "greeting": "Hello, Mollie Turner! You have 2 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "index": 1,
+      "guid": "98f1e5c5-9683-4006-9b6c-6b6422064195",
+      "isActive": false,
+      "balance": "$3,851.65",
+      "picture": "http://placehold.it/32x32",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Sherry Maldonado",
+      "gender": "female",
+      "company": "REMOLD",
+      "email": "sherrymaldonado@remold.com",
+      "phone": "+1 (818) 544-3428",
+      "address": "877 Bethel Loop, Bowmansville, Maryland, 8820",
+      "about": "Ad eu officia consectetur elit esse occaecat non nostrud sunt proident est et. Eu in quis do in irure anim. Do in officia et velit eiusmod aliquip magna incididunt laborum ut exercitation nostrud. Consequat nisi deserunt ea quis cillum do occaecat tempor esse ullamco occaecat culpa non sunt. Ea non commodo non exercitation cupidatat dolor sunt ut. Aliquip amet elit proident enim aliqua do sunt reprehenderit labore proident sint mollit. Nisi sint et magna adipisicing enim est quis.\r\n",
+      "registered": "2021-02-11T10:24:34 +03:00",
+      "latitude": -46.692857,
+      "longitude": 88.673713,
+      "tags": [
+        "occaecat",
+        "commodo",
+        "deserunt",
+        "laborum",
+        "amet",
+        "culpa",
+        "Lorem"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Oliver Ellison"
+        },
+        {
+          "id": 1,
+          "name": "Boyd Mcclure"
+        },
+        {
+          "id": 2,
+          "name": "Lorene Hebert"
+        }
+      ],
+      "greeting": "Hello, Sherry Maldonado! You have 9 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "index": 2,
+      "guid": "15054251-13c4-4f44-883c-7420c09a497e",
+      "isActive": true,
+      "balance": "$3,438.44",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Lori Mullins",
+      "gender": "female",
+      "company": "BARKARAMA",
+      "email": "lorimullins@barkarama.com",
+      "phone": "+1 (941) 513-3738",
+      "address": "180 Plaza Street, Glidden, Arkansas, 7705",
+      "about": "Consectetur Lorem voluptate aute deserunt veniam. Laborum qui est culpa id dolore. Quis exercitation ea anim ex qui proident voluptate ut velit exercitation. Do ipsum ad deserunt nostrud pariatur voluptate culpa eu eiusmod id.\r\n",
+      "registered": "2017-12-15T07:04:53 +03:00",
+      "latitude": -30.358245,
+      "longitude": -88.561844,
+      "tags": [
+        "irure",
+        "consectetur",
+        "id",
+        "amet",
+        "eiusmod",
+        "consequat",
+        "adipisicing"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Andrea Shannon"
+        },
+        {
+          "id": 1,
+          "name": "Noelle Christensen"
+        },
+        {
+          "id": 2,
+          "name": "Carney Cooper"
+        }
+      ],
+      "greeting": "Hello, Lori Mullins! You have 2 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "index": 3,
+      "guid": "7b118ff2-2619-44c9-b681-5ef01e5aef45",
+      "isActive": false,
+      "balance": "$3,382.95",
+      "picture": "http://placehold.it/32x32",
+      "age": 22,
+      "eyeColor": "brown",
+      "name": "Owen Kent",
+      "gender": "male",
+      "company": "EVEREST",
+      "email": "owenkent@everest.com",
+      "phone": "+1 (974) 548-2180",
+      "address": "275 Wortman Avenue, Brady, Rhode Island, 1435",
+      "about": "Quis fugiat sunt aute Lorem nisi. Officia ipsum elit occaecat deserunt et Lorem ipsum eu. Eu minim magna aliqua qui voluptate proident dolore sunt. Lorem nulla sit irure Lorem dolore ipsum quis sit dolore labore ex. Dolor est minim elit cupidatat reprehenderit quis nostrud nostrud do veniam aliqua exercitation nostrud ex.\r\n",
+      "registered": "2014-08-17T06:30:18 +03:00",
+      "latitude": -81.333667,
+      "longitude": -35.889353,
+      "tags": [
+        "nostrud",
+        "reprehenderit",
+        "in",
+        "laboris",
+        "culpa",
+        "do",
+        "consequat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Barbara Phillips"
+        },
+        {
+          "id": 1,
+          "name": "Goodman Keith"
+        },
+        {
+          "id": 2,
+          "name": "Ruby Hogan"
+        }
+      ],
+      "greeting": "Hello, Owen Kent! You have 10 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "index": 4,
+      "guid": "8b8e66d6-3b35-4bf5-a555-850d3ac572e9",
+      "isActive": false,
+      "balance": "$3,809.78",
+      "picture": "http://placehold.it/32x32",
+      "age": 35,
+      "eyeColor": "green",
+      "name": "Velma Burks",
+      "gender": "female",
+      "company": "ENERSOL",
+      "email": "velmaburks@enersol.com",
+      "phone": "+1 (829) 413-2791",
+      "address": "118 Heyward Street, Catharine, South Dakota, 258",
+      "about": "Ad in deserunt proident et in deserunt reprehenderit sint velit nulla et. Consectetur quis veniam ipsum esse commodo laborum officia mollit ad laboris cillum cupidatat enim ullamco. Quis laborum nostrud dolore quis occaecat laboris eu. Enim culpa est ut nulla non dolor in id Lorem voluptate consequat. Ullamco sit et nostrud dolor aute sit eu cupidatat in irure.\r\n",
+      "registered": "2017-05-17T11:12:58 +03:00",
+      "latitude": 76.999532,
+      "longitude": 82.397806,
+      "tags": [
+        "culpa",
+        "reprehenderit",
+        "Lorem",
+        "Lorem",
+        "proident",
+        "ipsum",
+        "et"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Antonia Good"
+        },
+        {
+          "id": 1,
+          "name": "Hunter Sampson"
+        },
+        {
+          "id": 2,
+          "name": "Bridget Baxter"
+        }
+      ],
+      "greeting": "Hello, Velma Burks! You have 10 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "index": 5,
+      "guid": "c478087d-3013-437c-ac23-ffeb5b8466a3",
+      "isActive": true,
+      "balance": "$2,908.63",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "green",
+      "name": "Constance Rodgers",
+      "gender": "female",
+      "company": "ERSUM",
+      "email": "constancerodgers@ersum.com",
+      "phone": "+1 (979) 589-2558",
+      "address": "787 Douglass Street, Comptche, Arizona, 5252",
+      "about": "Reprehenderit ipsum nulla esse quis laboris eiusmod ipsum aliqua ullamco irure Lorem eiusmod. Qui aute velit elit consectetur magna culpa nostrud consequat aliqua elit cillum. Magna dolor deserunt voluptate non consequat id in deserunt anim sunt cillum aliquip. Ea elit labore quis quis in.\r\n",
+      "registered": "2017-06-12T05:53:45 +03:00",
+      "latitude": -61.944894,
+      "longitude": 156.390796,
+      "tags": [
+        "reprehenderit",
+        "ea",
+        "do",
+        "cupidatat",
+        "veniam",
+        "id",
+        "consequat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Woodward Randolph"
+        },
+        {
+          "id": 1,
+          "name": "Terra Carr"
+        },
+        {
+          "id": 2,
+          "name": "Maxine Bridges"
+        }
+      ],
+      "greeting": "Hello, Constance Rodgers! You have 7 unread messages.",
+      "favoriteFruit": "banana"
+    }
+  ]
+
