6800f1bb5231e91bd7454f116b6dbb70826c65e4
hellshi <hcslimaa@gmail.com>
2023-02-28 08:24:54 -0300

    featt: multiplataform code with abstract factory

--- modulo06/creational/02.abstract-factory/plataforms/console/table.mjs
+++ modulo06/creational/02.abstract-factory/plataforms/console/table.mjs
@@ -2,6 +2,6 @@ import TableComponent from "../../shared/base/tableComponent.mjs";
 
 export default class TableConsoleComponent extends TableComponent {
     render(data) {
-        console.log("chamouuu", data)
+        console.log("chamouuu console", data)
     }
 }
