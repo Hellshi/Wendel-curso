bab91814bd09f6c8ad2b67ebb7c4738bec8b0fd0
hellshi <hcslimaa@gmail.com>
2023-03-12 10:09:37 -0300

    feat: first npm package

---
+++ modulo08/01-my-first-npm-module/myapp/index.js
@@ -0,0 +1,13 @@
+import FluentSQLBuilder from "@hellshi/fluentsql";
+
+import database from './database/data.json'  assert { type: "json" };
+
+const result = FluentSQLBuilder.for(database)
+                        .where({ registered: /^(2020|2019)/ })
+                        .select(['category'])
+                        .limit(3)
+                        .countBy('category')
+                        //.groupCount('category')
+                        .build()
+
+console.log(result)
